b01dfd7fd7c55a3a6166c7c6cf224cf35569bd33
HUI Ka Kuen <68351226+NelsonDevHK@users.noreply.github.com>
2026-08-13 03:12:21 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word = ""
+        for c in s:
+            if c.isalnum():
+                word += c.lower()
+        return word == word[::-1]
